8cff0c1e20a48c4fa69490244752c7361c26d291
SayanAcharya <rowdysayan99@gmail.com>
2017-12-31 04:53:16 +0000

    Update makefile

--- Android.mk
+++ Android.mk
@@ -1,7 +1,3 @@
 ifneq ($(filter msm8937,$(TARGET_BOARD_PLATFORM)),)
-
-ifeq ($(call is-vendor-board-platform,QCOM),true)
 include $(call all-named-subdir-makefiles,libbt-vendor)
 endif # is-vendor-board-platform
-
-endif
